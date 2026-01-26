d121596cc44837780b6e2d739ed0171b9f689c5a
se-akshay <akshaysingh6225808@gmail.com>
2026-01-26 12:20:07 +0530

    392. Is Subsequence

---
+++ Two Pointers/392. Is Subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while(i<t.size() && j < s.size()){
+            if(t[i] == s[j]){
+                j++;
+            }
+            i++;
+        }
+        return j == s.size();
+    }
+};
